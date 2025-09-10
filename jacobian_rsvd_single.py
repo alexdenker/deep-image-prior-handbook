FIX: imports for jacobian calculation

jacobian_rsvd_single.py
@@ -1,17 +1,15 @@
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
 import matplotlib.pyplot as plt 
-from skimage.metrics import peak_signal_noise_ratio
 from tqdm import tqdm
 from deepinv.physics import Tomography
 
 from torch.func import jvp, functional_call, vjp
 
-from physics.operator_module import OperatorModule
-from model.autoencoder import Autoencoder
+
+
+from dip import OperatorModule, get_unet_model
 
 cfg = {
     "forward_operator": "radon",  #"downsampling", # "radon"
@@ -20,61 +18,6 @@ cfg = {
     "model_inp": "fbp", # "random" "fbp"
 }
 
-
-class UNet(nn.Module):
-    def __init__(self):#, mean, std):
-        super(UNet, self).__init__()
-        
-        # Encoder
-        self.conv1 = nn.Conv2d(1, 128, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.conv4_b = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-
-        # Decoder
-        self.upconv1 = nn.Conv2d(128 + 128, 128, kernel_size=3, padding=1)  # concat with conv3 output
-        self.upconv2 = nn.Conv2d(128 + 128, 128, kernel_size=3, padding=1)  # concat with conv2 output
-        self.upconv3 = nn.Conv2d(128 + 128, 128, kernel_size=3, padding=1)  # concat with conv1 output
-        self.final_conv = nn.Conv2d(128, 1, kernel_size=1) 
-        self.relu = nn.ReLU(inplace=True)
-        #self.sigmoid = nn.Sigmoid() 
-
-        self._init_weights()
-    
-    def _init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.zeros_(m.bias)
-    
-    def forward(self, x):
-        # Encoder
-        x1 = self.relu(self.conv1(x))  # convd1 + relu1
-        x1_down = F.interpolate(x1, scale_factor=0.5, mode='bilinear', align_corners=True)  # down1
-
-        x2 = self.relu(self.conv2(x1_down))  # convd2 + relu2
-        x2_down = F.interpolate(x2, scale_factor=0.5, mode='bilinear', align_corners=True)  # down2
-
-        x3 = self.relu(self.conv3(x2_down))  # convd3 + relu3
-        x3_down = F.interpolate(x3, scale_factor=0.5, mode='bilinear', align_corners=True)  # down3
-
-        x4 = self.relu(self.conv4(x3_down))  # convd4 + relu4
-        x4 = self.relu(self.conv4_b(x4))     # conv4
-
-        # Decoder
-        x_up1 = F.interpolate(x4, scale_factor=2.0, mode='bilinear', align_corners=True)  # up1
-        x_up1 = self.relu(self.upconv1(torch.cat([x_up1, x3], dim=1)))  # skip connection from x3
-
-        x_up2 = F.interpolate(x_up1, scale_factor=2.0, mode='bilinear', align_corners=True)  # up2
-        x_up2 = self.relu(self.upconv2(torch.cat([x_up2, x2], dim=1)))  # skip connection from x2
-
-        x_up3 = F.interpolate(x_up2, scale_factor=2.0, mode='bilinear', align_corners=True)  # up3
-        x_up3 = self.relu(self.upconv3(torch.cat([x_up3, x1], dim=1)))  # skip connection from x1
-
-        out = self.final_conv(x_up3)  # convu4
-        return out #self.sigmoid(out)
 
 
 device = "cuda"
@@ -147,11 +90,8 @@ x_recos = []
 singular_values = [] 
 singular_vectors = [] 
 
-
-#model = UNet(mean=torch.mean(x).item(), std=torch.std(x).item()) 
-model = UNet()
-#model = Autoencoder(channels=512, padding_mode="circular", use_norm=True)
-
+model = get_unet_model(in_ch=1, out_ch=1, scales=6,
+                            skip=64, channels=(64,64,64,64,64,64), use_norm=False, use_sigmoid=False)
 model.to(device)   
 model.eval() 
 