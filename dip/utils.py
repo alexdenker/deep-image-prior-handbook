3e39baf05377119f9f445cdc6c19eb34a9d70c64
alexdenker <adenker@uni-bremen.de>
2025-09-11 07:58:08 +0100

    move rest of the utils

--- dip/utils.py
+++ dip/utils.py
@@ -1,4 +1,5 @@
 from types import SimpleNamespace
+import torch 
 
 def dict_to_namespace(d):
     if isinstance(d, dict):
@@ -7,4 +8,26 @@ def dict_to_namespace(d):
         return [dict_to_namespace(i) for i in d]
     else:
         return d
-    
+
+
+def make_serializable(obj):
+    if isinstance(obj, torch.nn.Parameter):
+        return obj.detach().cpu().item() if obj.numel() == 1 else obj.detach().cpu().tolist()
+
+    elif isinstance(obj, torch.Tensor):
+        return obj.item() if obj.numel() == 1 else obj.tolist()
+
+    elif isinstance(obj, SimpleNamespace):
+        return {"__namespace__": {k: make_serializable(v) for k, v in vars(obj).items()}}
+
+    elif isinstance(obj, dict):
+        return {k: make_serializable(v) for k, v in obj.items()}
+
+    elif isinstance(obj, (list, tuple)):
+        return [make_serializable(v) for v in obj]
+
+    elif isinstance(obj, (int, float, str, bool)) or obj is None:
+        return obj
+
+    else:
+        return str(obj)
